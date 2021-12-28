d8d6b46041705de658cce7c99d450e1478e407fa
Alesha Biryulin <76524913+RaymanPython@users.noreply.github.com>
2021-12-28 04:18:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 
+
 import pygame
+import pygame_gui
 import geometry_object
 import random
 
@@ -22,6 +24,8 @@ class Line(geometry_object.Line):
         self.B = b
         self.init()
         self.h = 5
+        self.show = True
+        self.remove = True
 
     def get_cor(self, x):
         return (x, self.y(x))
@@ -36,7 +40,7 @@ class Line(geometry_object.Line):
             p1, p2 = self.get_cor(-60), self.get_cor(1000)
         # pygame.draw.line(screen, self.color, (self.A.x, self.A.y), (self.B.x, self.B.y), 5)
         pygame.draw.line(screen, self.color, p1, p2, self.h)
-        # print(p1, p2)
+        # # # print(p1, p2)
 
     def init(self):
         try:
@@ -45,7 +49,8 @@ class Line(geometry_object.Line):
             r = a.dist(b)
             super().__init__(a, b)
         except:
-            print(all.point[self.A])
+            pass
+            # # print(all.point[self.A])
 
 
 class Circle(geometry_object.Circle):
@@ -56,6 +61,8 @@ class Circle(geometry_object.Circle):
         self.B = b
         self.init()
         self.h = 5
+        self.show = True
+        self.remove = True
 
     def get_cor(self, x):
         return (x, self.y(x))
@@ -63,7 +70,7 @@ class Circle(geometry_object.Circle):
     def draw(self, screen):
         # pygame.draw.line(screen, self.color, (self.A.x, self.A.y), (self.B.x, self.B.y), 5)
         pygame.draw.circle(screen, self.color, (self.x,  self.y), self.r, self.h)
-        # print(p1, p2)
+        # # # print(p1, p2)
 
     def init(self):
         a = point_to_geometry(all.point[self.A])
@@ -71,6 +78,38 @@ class Circle(geometry_object.Circle):
         r = a.dist(b)
         super().__init__(a.x, a.y, r)
 
+
+class Triangle(geometry_object.Triangle):
+
+    def __init__(self, a, b=None, c=None):
+        self.A = a
+        self.B = b
+        self.C = c
+        self.h = 5
+        self.color = BLACK
+        self.show = True
+        self.init()
+        self.remove = True
+
+    def draw(self, screen):
+        for i in range(3):
+            a = self.p[i]
+            b = self.p[(i + 1) % 3]
+            pygame.draw.line(screen, self.color, (a.x, a.y), (b.x,b.y), self.h)
+
+    def init(self):
+        if self.B == None and self.C == None:
+            a = point_to_geometry(all.point[self.A])
+            super().__init__(a, a, a)
+        elif self.B != None and self.C == None:
+            a = point_to_geometry(all.point[self.A])
+            b = point_to_geometry(all.point[self.B])
+            super().__init__(a, b, b)
+        else:
+            a = point_to_geometry(all.point[self.A])
+            b = point_to_geometry(all.point[self.B])
+            c = point_to_geometry(all.point[self.C])
+            super().__init__(a, b, c)
 
 
 
@@ -84,6 +123,7 @@ class Point(geometry_object.Point):
         super().__init__(x, y)
         # self.r = random.choice([5,10, 15, 20])
         self.r = 5
+        self.show = True
 
     def draw(self, screen):
         if self.move:
@@ -91,6 +131,8 @@ class Point(geometry_object.Point):
         else:
             self.color = GREEN
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+
+
 
 
 WIDTH = 700 # ширина игрового окна
@@ -144,26 +186,15 @@ class All:
         '''
 
     def __str__(self):
-        print(self.point)
+        # # print(self.point)
         return '5'
 
     def remove(self, ind):
-        self.all_sprites[ind] = None
+        if self.all_sprites[ind].remove:
+            self.all_sprites[ind] = None
 
     def remove_point(self, ind):
         self.point[ind] = Point(-100, -100)
-
-    def reinit(self, index):
-        for i in self.point_conect[index]:
-            if type(self.all_sprites[i]) != type(None):
-                for j in range(self.__len__()):
-                    if type(all.all_sprites[j]) == Point:
-                        if i in self.point_conect[j]:
-                            a = all.all_sprites[i]
-                            a = geometry_object.Point(a.x, a.y)
-                            b = all.all_sprites[j]
-                            b = geometry_object.Point(b.x, b.y)
-                            all.all_sprites[i].__init__(a, b)
 
     def upted(self):
         for i in range(self.__len__()):
@@ -176,15 +207,16 @@ class All:
     def sum(self, i , j):
         for k in range(len(self.all_sprites)):
             a = self.all_sprites[k]
+            if type(a) == Triangle:
+                if self.all_sprites[k].C == j:
+                    self.all_sprites[k].C = i
             if self.all_sprites[k].A == j:
-                print(True, True)
+                # # print(True, True)
                 self.all_sprites[k].A = i
             if self.all_sprites[k].B == j:
-                print(True, False, k, j, i)
+                # # print(True, False, k, j, i)
                 self.all_sprites[k].B = i
-        print(list(map(str, self.point)))
-        for i in self.all_sprites:
-            print(i.A, i.B)
+        # # print(list(map(str, self.point)))
 
     def upted_index(self, index):
         remove = False
@@ -205,16 +237,35 @@ class All:
                 self.all_sprites[i].init()
 
 
-                
+
 
 
 # Создаем игру и окно
 pygame.init()
-pygame.mixer.init()
+
+pygame.display.set_caption('Mini_Geogebra')
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+
+background = pygame.Surface((WIDTH, HEIGHT))
+background.fill(pygame.Color('#000000'))
+
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+time_delta = clock.tick(30) / 1000.0
+text = ['прямая', 'окружность', 'треугольник', 'точка', 'удаление', 'переместить']
+text_object = ['line', 'circle', 'triangle', 'point', 'remove', 'poisk']
+button = []
+wb = 100
+hb = 50
+for i in range(len(text)):
+    button.append(pygame_gui.elements.UIButton(relative_rect=pygame.Rect((i * wb + 1, 0), (wb - 2, hb - 2)),
+                                            text=text[i],
+                                            manager=manager)
+                  )
 # all_sprites = []
 
 # Цикл игры
@@ -222,7 +273,7 @@ clock = pygame.time.Clock()
 running = True
 
 mouse_cors = []
-object_type = 'line'
+object_type = ''
 sprite_move = -1
 index_move = -1
 all = All()
@@ -233,12 +284,14 @@ for i in range(20):
     line = Line(ind1, ind2)
     line.color = BLUE
     line.h = 2
+    line.remove = False
     all.all_sprites.append(line)
     ind1 = all.add_point(delta * i, -100)
     ind2 = all.add_point(delta * i, -50)
     line = Line(ind1, ind2)
     line.color = BLUE
     line.h = 2
+    line.remove = False
     all.all_sprites.append(line)
 while running:
     # Держим цикл на правильной скорости
@@ -248,6 +301,11 @@ while running:
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                for i in range(len(button)):
+                    if event.ui_element == button[i]:
+                        object_type = text_object[i]
         if event.type == pygame.KEYDOWN:
             # If pressed key is ESC quit program
             if event.key == pygame.K_1:
@@ -258,12 +316,17 @@ while running:
                 object_type = 'point'
             elif event.key == pygame.K_4:
                 object_type = 'circle'
+            elif event.key == pygame.K_5:
+                object_type = 'triangle'
             elif event.key == pygame.K_r:
                 object_type = 'remove'
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if index_move != -1:
+            if event.pos[1] <= hb:
+                pass
+            # print(object_type)
+            elif index_move != -1:
                 pos = event.pos
-                print(index_move)
+                # # print(index_move)
                 if type(all.point[index_move]) == Point:
                     all.point[index_move].__init__(pos[0], pos[1])
                     all.upted_index(index_move)
@@ -282,7 +345,7 @@ while running:
                     pos = event.pos
                     all.point[all.all_sprites[-1].A].__init__(a[0], a[1])
                     all.point[all.all_sprites[-1].B].__init__(pos[0], pos[1])
-
+                    all.upted_index(all.all_sprites[-1].B)
                     all.all_sprites[-1].init()
                     mouse_cors = []
             elif object_type == 'circle':
@@ -299,12 +362,35 @@ while running:
                     pos = event.pos
                     all.point[all.all_sprites[-1].A].__init__(a[0], a[1])
                     all.point[all.all_sprites[-1].B].__init__(pos[0], pos[1])
+                    all.upted_index(all.all_sprites[-1].B)
                     all.all_sprites[-1].init()
                     mouse_cors = []
-                # print(pos)
+                # # # print(pos)
+            elif object_type == 'triangle':
+                # print(object_type, 5)
+                if len(mouse_cors) == 0:
+                    pos = event.pos
+                    ind1 = all.add_point(pos[0], pos[1])
+                    ind2 = all.add_point(pos[0] + 10, pos[1])
+                    tr = Triangle(ind1, ind2)
+                    all.add_object(tr)
+                    mouse_cors.append(pos)
+                elif len(mouse_cors) == 1:
+                    pos = event.pos
+                    ind2 = all.add_point(pos[0] + 10, pos[1])
+                    all.point[all.all_sprites[-1].B].__init__(pos[0], pos[1])
+                    all.all_sprites[-1].C = ind2
+                    all.upted_index(all.all_sprites[-1].B)
+                    mouse_cors.append(pos)
+                elif len(mouse_cors) == 2:
+                    pos = event.pos
+                    all.point[all.all_sprites[-1].C].__init__(pos[0], pos[1])
+                    all.upted_index(all.all_sprites[-1].C)
+                    mouse_cors = []
             elif object_type == 'point':
                 pos = event.pos
                 all.add_point(pos[0], pos[1])
+
             elif object_type == 'poisk':
                 pos = event.pos
                 for i in range(len(all)):
@@ -316,33 +402,51 @@ while running:
             elif object_type == 'remove':
                 pos = event.pos
                 p = geometry_object.Point(pos[0], pos[1])
-                for i in range(len(all)):
+                for i in range(len(all.all_sprites)):
                     if type(all.all_sprites[i]) != Point and type(all.all_sprites[i]) != type(None):
-                        print(i, 'com', type(all.all_sprites[i]))
+                        # # # print(i, 'com', type(all.all_sprites[i]))
                         if all.all_sprites[i].in_to(p, 5):
                             all.remove(i)
-                            print(i)
+                            # # print(i)
                             all.all_sprites[i] = None
                         # all.all_sprites[i] = None
+                for i in range(len(all.point)):
+                    if type(all.point[i]) == Point and type(all.point[i]) != type(None):
+                        # # # print(i, 'com', type(all.all_sprites[i]))
+                        if all.point[i].in_to(p, 5):
+                            all.remove_point(i)
+
 
         elif event.type == pygame.MOUSEMOTION:
-            if object_type != None and object_type != 'point':
+            if object_type == 'triangle':
+                if len(mouse_cors) == 0:
+                    pass
+                elif all.all_sprites[-1].C == None:
+                    b = event.pos
+                    all.point[all.all_sprites[-1].B].__init__(b[0], b[1])
+                    all.all_sprites[-1].init()
+                else:
+                    b = event.pos
+                    all.point[all.all_sprites[-1].C].__init__(b[0], b[1])
+                    all.all_sprites[-1].init()
+            elif object_type != None and object_type != 'point':
                 if len(mouse_cors) == 1:
                     a = mouse_cors[0]
                     b = event.pos
-                    all.point[all.all_sprites[-1].A].__init__(a[0], a[1])
+                    # all.point[all.all_sprites[-1].A].__init__(a[0], a[1])
                     all.point[all.all_sprites[-1].B].__init__(b[0], b[1])
-                    all.upted_index(all.all_sprites[-1].A)
-                    all.upted_index(all.all_sprites[-1].B)
+                    # all.upted_index(all.all_sprites[-1].A)
+                    # all.upted_index(all.all_sprites[-1].B)
                     all.all_sprites[-1].init()
                     # mouse_cors = []
             if index_move != -1:
                 pos = pygame.mouse.get_pos()
-                # print(index_move)
+                # # # print(index_move)
                 if type(all.point[index_move]) == Point:
                     all.point[index_move].__init__(pos[0], pos[1])
 
-
+        manager.process_events(event)
+    manager.update(time_delta)
     # Обновление
 
     # Обновление
@@ -356,14 +460,17 @@ while running:
     for i in all.sprites():
         if type(i) == type(None):
             continue
-        i.draw(screen)
-        # print(i)
+        if i.show:
+            i.draw(screen)
+        # # # print(i)
     clock.tick(30)
+
+    manager.draw_ui(screen)
     # Обновляем экран после рисования объектов
     pygame.display.flip()
 pygame.quit()
-print(all.all_sprites)
-print(all)
+# # print(all.all_sprites)
+# # print(all)
 
 
 
