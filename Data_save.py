997529ab41c962aab5135d6b0d3839c31068857e
Alesha Biryulin <76524913+RaymanPython@users.noreply.github.com>
2022-01-02 16:07:29 +0300

    Add files via upload

---
+++ Data_save.py
@@ -0,0 +1,35 @@
+import tkinter.filedialog as fd
+
+TYPE_GEO_PY = '.geopy'
+filetypes = ("Геогебра обьект", TYPE_GEO_PY),
+
+def save(all):
+    new_file = fd.asksaveasfile(title="Сохранить файл", defaultextension=TYPE_GEO_PY,
+                                filetypes=(filetypes))
+    if new_file == None:
+        return
+
+    for i in all.point:
+        new_file.write(f'all.add_point({str(i)})' + '\n')
+        arg = i.__dict__
+        #print(type(i).__getnewargs__())
+        for j in arg:
+            new_file.write(f'all.point[-1].__setattr__("{j}", {arg[j]})' + '\n')
+    for i in all.all_sprites:
+        new_file.write(f'all.add_object({str(i)})' + '\n')
+        arg = i.__dict__
+        # print(i.__getinitargs__())
+        for j in arg:
+            new_file.write(f'all.all_sprites[-1].__setattr__("{j}", {arg[j]})' + '\n')
+
+    new_file.close()
+
+def open():
+    my_file = fd.askopenfile(title="Открыть файл", filetypes=filetypes)
+    s = ''
+    if my_file:
+        s = my_file.readlines()
+        my_file.close()
+    return s
+
+
